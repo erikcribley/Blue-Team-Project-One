c48b6d724627a9abc92b1feba990d7e1a254421e
Nathan Visti <nathanvisti@gmail.com>
2019-06-25 19:04:31 -0400

    Created ajax call

--- javascript.js
+++ javascript.js
@@ -3,3 +3,14 @@
 
 
 // API work below
+
+function loadDoc() {
+    var response = 'fiction'
+    var queryURL = "https://api.nytimes.com/svc/books/v3/lists/current/hardcover-" + response + ".json?api-key=YYLM4H4ZVCvkU3CCEM6hMcTnfTjAP7DM"
+    console.log(queryURL)
+
+    $.ajax({
+        url: queryURL
+    })
+
+}
