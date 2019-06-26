9a75cd174ecbed028eb57df60b00d86af99ad1e7
Nathan Visti <nathanvisti@gmail.com>
2019-06-25 20:14:43 -0400

    Changed function name

--- javascript.js
+++ javascript.js
@@ -4,13 +4,13 @@
 
 // API work below
 
-function loadDoc() {
+function findBook() {
     var response = 'fiction'
     var queryURL = "https://api.nytimes.com/svc/books/v3/lists/current/hardcover-" + response + ".json?api-key=YYLM4H4ZVCvkU3CCEM6hMcTnfTjAP7DM"
     console.log(queryURL)
 
-    $.ajax({
-        url: queryURL
-    })
+    // $.ajax({
+    //     url: queryURL
+    // })
 
 }
