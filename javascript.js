b83defaef9bc869ab22c345996089e41d97aadd2
Erik Cribley <erikcribley@gmail.com>
2019-06-25 20:00:55 -0400

    added link to javascript to index, added  click event

--- javascript.js
+++ javascript.js
@@ -1,9 +1,10 @@
 // Plain old JS up here
-
-
+$('button').on("click", function() {
+    let response = $('select').val().trim()
+    console.log(genre)
+})
 
 // API work below
-
 function loadDoc() {
     var response = 'fiction'
     var queryURL = "https://api.nytimes.com/svc/books/v3/lists/current/hardcover-" + response + ".json?api-key=YYLM4H4ZVCvkU3CCEM6hMcTnfTjAP7DM"
