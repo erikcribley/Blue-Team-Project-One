bb4f7d5d67c3c6a1e1e3c41d991a3652b062f2da
Erik Cribley <erikcribley@gmail.com>
2019-06-27 21:22:47 -0400

    json values in html on click event

--- javascript.js
+++ javascript.js
@@ -1,17 +1,32 @@
-
-var response = ''
+var category = ''
 
 $('button').on("click", function loadDoc() {
-    let response = $('select').val().trim()
-    console.log(response)
-    var queryURL = "https://api.nytimes.com/svc/books/v3/lists/" + response + ".json?api-key=YYLM4H4ZVCvkU3CCEM6hMcTnfTjAP7DM"
+    let category = $('select').val().trim()
+    console.log(category)
+    var queryURL = "https://api.nytimes.com/svc/books/v3/lists/" + category + ".json?api-key=YYLM4H4ZVCvkU3CCEM6hMcTnfTjAP7DM"
     console.log(queryURL)
 // ====== need to pull a response from a book
     $.ajax({
         url: queryURL,
-       method: "GET" 
+        method: "GET"
     }).then(function(response){
         console.log(response)
-
+        let genre = response.results.list_name;
+        let heading = $("<h2>")
+                .text(genre)
+        $(".bookReturn")
+            .append(heading)
+        for (i = 0; i < response.results.books.length; i++) {
+            let author = response.results.books[i].author
+            let title = response.results.books[i].title
+            let coverImage = response.results.books[i].book_image
+            let image = $("<img>")
+                .attr("src", coverImage)
+            let bookInfo = $("<p>")
+                .text(title + " by " + author)
+        $(".bookReturn")
+            .append(image)
+            .append(bookInfo)
+        }
     })
 })
